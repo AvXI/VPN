71cb6574d00f7d9d29aaf789445d7123ead381d4
AvXI <88413258+AvXI@users.noreply.github.com>
2024-07-04 10:15:32 +0300

    Add files via upload

---
+++ OpenVPN.py
@@ -0,0 +1,39 @@
+import subprocess
+
+# Install OpenVPN
+subprocess.run(['sudo', 'apt-get', 'install', 'openvpn', '-y'])
+
+# Generate server keys
+subprocess.run(['sudo', 'openvpn', '--genkey', '--secret', 'server.key'])
+
+# Configure server
+server_conf = """
+port 1194
+proto udp
+dev tun
+ca ca.crt
+cert server.crt
+key server.key
+dh dh.pem
+server 10.8.0.0 255.255.255.0
+ifconfig-pool-persist ipp.txt
+push "redirect-gateway def1 bypass-dhcp"
+push "dhcp-option DNS 8.8.8.8"
+keepalive 10 120
+tls-auth ta.key 0
+cipher AES-256-CBC
+auth SHA256
+user nobody
+group nogroup
+persist-key
+persist-tun
+status openvpn-status.log
+verb 3
+"""
+
+with open('/etc/openvpn/server.conf', 'w') as f:
+    f.write(server_conf)
+
+# Start OpenVPN
+subprocess.run(['sudo', 'systemctl', 'enable', 'openvpn.service'])
+subprocess.run(['sudo', 'systemctl', 'start', 'openvpn.service'])

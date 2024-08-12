91120a9fede3ddab95818705318f61b483da7319
AvXI <88413258+AvXI@users.noreply.github.com>
2024-08-12 09:09:25 +0300

    Update OpenVPN.py

--- OpenVPN.py
+++ OpenVPN.py
@@ -18,7 +18,7 @@ dh dh.pem
 server 10.8.0.0 255.255.255.0
 ifconfig-pool-persist ipp.txt
 push "redirect-gateway def1 bypass-dhcp"
-push "dhcp-option DNS 8.8.8.8"
+push "dhcp-option DNS 9.9.9.11"
 keepalive 10 120
 tls-auth ta.key 0
 cipher AES-256-CBC
